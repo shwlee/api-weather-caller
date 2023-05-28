67d8e2261641dc5a18a295dd40e55441d17f306d
greg-clo <greg@clo3d.com>
2023-05-28 19:40:38 +0900

    add complete source

--- Weather-caller/Weather-caller/Program.cs
+++ Weather-caller/Weather-caller/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+const string baseUrl = "https://localhost:5001";
+var httpClient = new HttpClient();
+httpClient.BaseAddress = new Uri(baseUrl);
+var apiClient = new WeatherApiClient.Generation.WeatherApiClient(baseUrl, httpClient);
+var weathers = await apiClient.GetWeatherForecastAsync();
+
+foreach (var weather in weathers)
+{
+    Console.WriteLine($"{weather.Date}, {weather.Summary}, {weather.TemperatureC}, {weather.TemperatureF}");
+}
+
+Console.WriteLine();
+Console.WriteLine("DONE!");
